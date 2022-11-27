76b7bac3d2b803902c43ab286b27f631bd3922a6
Nebojsa Zoric <office@kolosek.com>
2022-11-28 00:49:57 +0100

    Fixed tests

--- spec/views/items/index.html.erb_spec.rb
+++ spec/views/items/index.html.erb_spec.rb
@@ -15,6 +15,6 @@ RSpec.describe "items/index", type: :view do
   it "renders a list of items on screen" do
     render
     cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
-    assert_select cell_selector, text: Regexp.new("Name".to_s), count: 3
+    assert_select cell_selector, text: Regexp.new("Name".to_s), count: 2
   end
 end
